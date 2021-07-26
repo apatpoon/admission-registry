05826e676c7eb7c642e5937b7c601196195c9aeb
Patrick Pan1 <patrick_pan1@epam.com>
2021-07-26 17:55:42 +0800

    'update'

--- pkg/mutate.go
+++ pkg/mutate.go
@@ -239,15 +239,17 @@ func updateDeploymentSpec(deploySpec appsv1.DeploymentSpec) (patch []patchOperat
 			patch = append(patch, patchOperation{
 				Op:   "add",
 				Path: "/spec/template/spec/containers",
-				Value: &corev1.Container{
-					Name:    SideCarContainerName,
-					Image:   "nginx:1.18.0",
-					Command: []string{"/bin/bash", "-c", "sleep", "6000"},
-					Ports: []corev1.ContainerPort{
-						{
-							Name:          "http",
-							ContainerPort: 80,
-							Protocol:      "TCP",
+				Value: []corev1.Container{
+					{
+						Name:    SideCarContainerName,
+						Image:   "nginx:1.18.0",
+						Command: []string{"/bin/bash", "-c", "sleep", "6000"},
+						Ports: []corev1.ContainerPort{
+							{
+								Name:          "http",
+								ContainerPort: 80,
+								Protocol:      "TCP",
+							},
 						},
 					},
 				},
