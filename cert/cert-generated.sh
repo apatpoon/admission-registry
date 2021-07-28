884b74d2e5d16dbc5ea005e31eac2d4c47843b49
Patrick Pan1 <patrick_pan1@epam.com>
2021-07-28 10:43:11 +0800

    cert generated supported

---
+++ cert/cert-generated.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+expireTime="87600h"
+service-name="admission-registry"
+service-namespace="default"
+wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+chmod +x cfssl_linux-amd64 cfssljson_linux-amd64
+sudo mv cfssl_linux-amd64 /usr/local/bin/cfssl
+sudo mv cfssljson_linux-amd64 /usr/local/bin/cfssljson
+
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": ${expireTime}
+    },
+    "profiles": {
+      "server": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": ${expireTime}
+      }
+    }
+  }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+    "CN": ${CN},
+    "key": {
+        "algo": "rsa",
+        "size": 2048
+    },
+    "names": [
+        {
+            "C": "CN",
+            "L": "BeiJing",
+            "ST": "BeiJing",
+            "O": "k8s",
+            "OU": "System"
+        }
+    ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+cat > server-csr.json <<EOF
+{
+  "CN": "admission",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+        "C": "CN",
+        "L": "BeiJing",
+        "ST": "BeiJing",
+        "O": "k8s",
+        "OU": "System"
+    }
+  ]
+}
+EOF
+
+# -hostname 的值，格式为  {service-name}.{service-namespace}.svc，其中 service-name 代表你 webhook 的 Service 名字，service-namespace 代表你 webhook 的命名空间。
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json \
+		-hostname="${service-name}"."${service-namespace}".svc -profile=server server-csr.json | cfssljson -bare server
+
+kubectl create secret tls admission-registry-tls \
+        --key=server-key.pem \
+        --cert=server.pem
+
+echo "Use this follow output to replace CA_BUNDLE in mutating | validating webhook yaml"
+cat ca.pem | base64
